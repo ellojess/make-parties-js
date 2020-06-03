6eb71d3c10782ed68c9d944723699ea2d40a35b1
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-06-02 23:52:44 -0700

    Users can create events

--- db/models/event.js
+++ db/models/event.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const Event = sequelize.define('Event', {
     title: DataTypes.STRING,
-    desc: DataTypes.TEXT
+    desc: DataTypes.TEXT,
+    imgUrl: DataTypes.STRING 
   }, {});
   Event.associate = function(models) {
     // associations can be defined here
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const bodyParser = require('body-parser');
 // require handlebars
 var exphbs = require('express-handlebars');
 
+// access models
+const models = require('./db/models');
+
 // OUR MOCK ARRAY OF PROJECTS
 var events = [
     { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
@@ -22,10 +25,12 @@ app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// INDEX
+// Index
 app.get('/', (req, res) => {
+  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
     res.render('events-index', { events: events });
   })
+})
 
 // NEW
 app.get('/events/new', (req, res) => {
@@ -42,5 +47,9 @@ app.listen(port, () => {
 
 // CREATE
 app.post('/events', (req, res) => {
-  console.log(req.body);
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+  }).catch((err) => {
+    console.log(err)
+  });
 })
